055103e762e491e05d0742f0e7ff9cdee663f321
Matej Konrad <matej.konrd@gmail.com>
2019-11-13 22:40:50 +0100

    Added hero api functionality and dashboard

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,24 @@
-import { HEROES } from "./../mock-heroes";
-import { Hero } from "../Hero";
-import { Component, OnInit } from "@angular/core";
+import { HeroService } from './../hero.service';
+import { HEROES } from './../mock-heroes';
+import { Hero } from '../Hero';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-heroes",
-  templateUrl: "./heroes.component.html",
-  styleUrls: ["./heroes.component.css"]
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
+
   heroes: Hero[] = HEROES;
 
-  constructor() {}
+  constructor(private heroService: HeroService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getHeroes();
+  }
 
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
   }
 }
